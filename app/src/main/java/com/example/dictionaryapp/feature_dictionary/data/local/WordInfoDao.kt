61fa6ce652f3874bdc95a1285234f36b86d0c4fe
introidx <prakash.r19500@gmail.com>
2023-04-08 11:21:00 +0530

    Final commit

--- app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/local/WordInfoDao.kt
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/local/WordInfoDao.kt
@@ -13,8 +13,8 @@ interface WordInfoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertWordInfos(infos : List<WordInfoEntity>)
 
-    @Query("DELETE FROM wordinfoentity WHERE word IN(:words")
-    suspend fun deleteWordInfos(words : List<String>)
+    @Query("DELETE FROM wordinfoentity WHERE word IN(:words)")
+    suspend fun deleteWordInfos(words: List<String>)
 
     @Query("SELECT * FROM wordinfoentity WHERE word LIKE '%' || :word || '%'")
     suspend fun getWordInfos(word : String) : List<WordInfoEntity>
